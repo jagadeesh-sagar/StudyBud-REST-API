50d6a41a83f388b1d4df5daa3bce35ffc18c3ced
G.Jagadeesh <161948918+jagadeesh-sagar@users.noreply.github.com>
2025-08-22 11:31:31 +0530

    Update settings.py

--- discord/discord/settings.py
+++ discord/discord/settings.py
@@ -65,7 +65,7 @@ CORS_ALLOWED_ORIGINS=[
 
 if DEBUG:
   CORS_ALLOWED_ORIGINS +=[
-    "http://localhost:8111", # not for production
+    "http://localhost:5173", # not for production
     "https://localhost:8111", # only allow ssl
   ]
 
