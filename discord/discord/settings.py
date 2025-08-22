1c65281126dc9d519f4477ed03cafb7981d1d459
G.Jagadeesh <161948918+jagadeesh-sagar@users.noreply.github.com>
2025-08-22 06:37:45 +0530

    Update settings.py

--- discord/discord/settings.py
+++ discord/discord/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-s#%jr!$ty*qmy)r6_96gb_0c#)qsm#zhx3eg6tn+kg35lem2-*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['13.234.124.118','localhost']
 
 
 # Application definition
